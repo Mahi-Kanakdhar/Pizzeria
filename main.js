4466d60dbdcb56d0e2f73751fd4dad10a9f6f989
Mahi Kanakdhar <85062554+Mahi-Kanakdhar@users.noreply.github.com>
2021-07-04 19:31:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Margherita Pizza","Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata;
+        htmldata="<ol class='menulist'>"
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+        }
+         htmldata=htmldata+"</ol>"
+         document.getElementById("display_menu").innerHTML = htmldata;
+    }
+
+    function add_item(){
+        var htmldata;
+        var item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata="<section class='cards'>"
+        for(var i=0;i<menu_list_array.length;i++){
+
+            htmldata=htmldata+'<div class="card">' +'<img src="https://oyalo.in/mob/img/pizza.png"/>' + menu_list_array[i] + '</div>'
+        }
+          htmldata=htmldata+"</section>"
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
